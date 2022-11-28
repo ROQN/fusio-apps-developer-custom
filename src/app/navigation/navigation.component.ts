7a6752375f9f4bebbb5a1d2a6225644fc45090a4
Denys <denys.vasylenko@sapient.pro>
2022-11-28 16:56:47 +0200

    fix style

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ElementRef, Renderer2} from '@angular/core';
 import {UserAccount} from "fusio-sdk/dist/src/generated/consumer/UserAccount";
 import {ConsumerService, UserService} from "ngx-fusio-sdk";
 
@@ -13,11 +13,21 @@ export class NavigationComponent implements OnInit {
   isMenuCollapsed = true;
   account?: UserAccount;
 
-  constructor(private consumer: ConsumerService, private user: UserService<UserAccount>) { }
+  constructor(private consumer: ConsumerService, private user: UserService<UserAccount>, private elem: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
     this.isAuthenticated = this.consumer.hasValidToken();
     this.account = this.user.get();
   }
 
+  setActive(ind: string) {
+    const rows = this.elem.nativeElement.querySelectorAll('.nav-link.custom-link-style')
+    for (const row of rows) {
+      row.classList.remove('active');
+    }
+    const row = this.elem.nativeElement.querySelector(`.nav-link-${ind}-js`)
+    if (row) {
+      row.classList.add('active');
+    }
+  }
 }
